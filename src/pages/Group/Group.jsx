f1651c971757b41eeaadfa9b843a4a18ba7fcf9c
Cuyqo <saon0410@gmail.com>
2024-09-30 17:26:24 +0900

    [Design] serach Section

--- src/pages/Group/Group.jsx
+++ src/pages/Group/Group.jsx
@@ -1,5 +1,12 @@
-import { GroupWrapper, TabBox } from './styles';
+import {
+  GroupWrapper,
+  SearchBox,
+  SearchInput,
+  SearchSection,
+  TabBox,
+} from './styles';
 import { useState } from 'react';
+import searchIcon from 'assets/Group/searchBar.svg';
 
 export const Group = () => {
   const [isLock, setIsLock] = useState('public');
@@ -10,20 +17,26 @@ export const Group = () => {
 
   return (
     <GroupWrapper>
-      <TabBox
-        aria-pressed={isLock === 'public'}
-        onClick={() => handleClick('public')}
-        $isLock={isLock === 'public' ? 'true' : 'false'}
-      >
-        공개
-      </TabBox>
-      <TabBox
-        aria-pressed={isLock === 'private'}
-        onClick={() => handleClick('private')}
-        $isLock={isLock === 'private' ? 'true' : 'false'}
-      >
-        비공개
-      </TabBox>
+      <SearchSection>
+        <TabBox
+          aria-pressed={isLock === 'public'}
+          onClick={() => handleClick('public')}
+          $isLock={isLock === 'public' ? 'true' : 'false'}
+        >
+          공개
+        </TabBox>
+        <TabBox
+          aria-pressed={isLock === 'private'}
+          onClick={() => handleClick('private')}
+          $isLock={isLock === 'private' ? 'true' : 'false'}
+        >
+          비공개
+        </TabBox>
+        <SearchBox>
+          <img src={searchIcon} style={{ backgroundColor: 'transparent' }} />
+          <SearchInput placeholder="그룹명을 검색해 주세요" />
+        </SearchBox>
+      </SearchSection>
     </GroupWrapper>
   );
 };
--- src/pages/Group/styles.js
+++ src/pages/Group/styles.js
@@ -10,10 +10,12 @@ export const SearchSection = styled.div`
   width: 100%;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-evenly;
+  gap: 10px;
 `;
 
 export const TabBox = styled.button`
+  ${(props) => props.theme.fontStyles.Bold14};
   width: 66px;
   height: 45px;
   border-radius: 22.5px;
@@ -23,4 +25,31 @@ export const TabBox = styled.button`
   background-color: ${({ theme, $isLock }) =>
     $isLock === 'true' ? `${theme.colors.black}` : 'transparent'};
   cursor: pointer;
+`;
+
+export const SearchBox = styled.div`
+  flex: 1;
+  border-radius: 6px;
+  padding: 10px 20px;
+  background-color: ${(props) => props.theme.colors.gray4};
+  display: flex;
+  gap: 10px;
+`;
+
+export const SearchInput = styled.input`
+  background-color: transparent;
+  border: none;
+  border-radius: 5px; // 테두리 둥글기
+  width: 100%;
+
+  ::placeholder {
+    ${(props) => props.theme.fontStyles.Regular14};
+    color: ${(props) => props.theme.colors.gray2};
+  }
+
+  &:focus {
+    outline: none; // 기본 outline 제거
+    /* box-shadow: 0 0 0 2px ${(props) =>
+      props.theme.colors.gray3}; // 포커스 시 box-shadow  } */
+  }
 `;
